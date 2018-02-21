af9bc38a64cb95f1f9a2689b6417c4e3259e3343
HarleySG <harley.santos.garzon@gmail.com>
2018-02-20 23:55:27 -0500

    new component ListTodo, estructura base, conexión con props y renderizxado de value default del arrTodo

---
+++ src/components/ListTodo.component.vue
@@ -0,0 +1,23 @@
+<template>
+	<section class="c-todo-section">
+		<ul class="c-todoApp-list">
+			<li class="c-todoApp-todo" v-for="(todo, indice) of listTodos" :key="indice">
+				<input type="checkbox"
+					name="descriptionTodo"
+					id="'thisTodo_'+indice"
+					:checked="(todo.stateTodo == true) ? 'checked' : ''"
+					class="c-todoApp-checkTodo">
+
+				<label for="'thisTodo_'+indice"
+					@click="todo.stateTodo = !todo.stateTodo"
+					v-text="todo.description"></label>
+			</li>
+		</ul>
+	</section>
+</template>
+
+<script>
+export default {
+	props: ['listTodos']
+}
+</script>
