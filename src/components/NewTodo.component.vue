0a9160a9e3f1fc0cd79e0ecf631918533c8a52bf
HarleySG <harley.santos.garzon@gmail.com>
2018-02-20 23:54:30 -0500

    new component, se define la conexión con los props y la estructura del arrTodos, sin contenido relevante

---
+++ src/components/NewTodo.component.vue
@@ -0,0 +1,25 @@
+<template>
+	<header>
+		<input type="text"
+					autofocus
+					v-model="newTodo">
+	</header>
+</template>
+
+<script>
+export default {
+	props: {
+		arrTodos: [
+			{
+				description: String,
+				stateTodo: Boolean
+			}
+		]
+	},
+	data() {
+		return {
+			newTodo: ''
+		}
+	}
+}
+</script>
