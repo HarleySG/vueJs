9bf8c3d1ea9edbc82db45b2ddc927e73ae224467
HarleySG <harley.santos.garzon@gmail.com>
2018-02-20 23:51:55 -0500

    Se importan los nuevos componentes y se define un arrTodo con un valor default - Se define algunos estilos base

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,43 @@
 <template>
-  <div id="app" v-cloak class="o-todo">
-     {{msg}}
-  </div>
+	<main id="app" v-cloak class="o-todo">
+		<div class="c-todoApp">
+			<header-todo></header-todo>
+			<new-todo></new-todo>
+			<list-todo :listTodos="arrTodos"></list-todo>
+		</div>
+	</main>
 </template>
 
 <script>
+import HeaderTodo from "./components/HeaderTodo.component.vue"
+import NewTodo from "./components/NewTodo.component.vue"
+import ListTodo from "./components/ListTodo.component.vue"
+
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
+	components: {
+		HeaderTodo,
+		NewTodo,
+		ListTodo
+	},
+	data () {
+		return {
+			arrTodos: [
+				{
+					description: 'Todo de ejemplo',
+					stateTodo: false
+				}
+			]
+		}
+	}
 }
 </script>
 
 <style lang="scss">
-
+	*, *::before, *::after{
+		box-sizing: border-box;
+	}
+	html{
+		font-family: 'Helvetica', Arial, sans-serif;
+		font-size: 16px;
+	}
 </style>
